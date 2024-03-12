cb60b75891142fbaeaa6b4df60282636f46aa829
Steven Arnott <steven.arnott@grantstreet.com>
2024-03-12 15:48:14 +0000

    Add missing fields for aler rule types

--- alert_rule.go
+++ alert_rule.go
@@ -30,9 +30,34 @@ type AlertRuleCondition struct {
 // Refer to https://github.com/getsentry/sentry/tree/master/src/sentry/rules/actions or GUI
 // to get detailed information.
 type AlertRuleAction struct {
-	ID      string `json:"id,omitempty"`
-	Name    string `json:"name,omitempty"`
-	Service string `json:"service,omitempty"`
+	ID               string `json:"id,omitempty"`
+	TargetType       string `json:"targetType,omitempty"`
+	FallThroughType  string `json:"fallthroughType,omitempty"`
+	TargetIdentifier uint   `json:"targetIdentifier,omitempty"`
+	Name             string `json:"name,omitempty"`
+	Service          string `json:"service,omitempty"`
+	Workspace        string `json:"workspace,omitempty"`
+	Channel          string `json:"channel,omitempty"`
+	ChannelID        string `json:"channel_id,omitempty"`
+	Tags             string `json:"tags,omitempty"`
+	Account          string `json:"account,omitempty"`
+}
+
+// AlertFilter represents alert filter
+// Refer to https://github.com/getsentry/sentry/tree/master/src/sentry/rules/filters or GUI
+// to get detailed information.
+type AlertFilters struct {
+	ID               string      `json:"id,omitempty"`
+	Name             string      `json:"name,omitempty"`
+	Attribute        string      `json:"attribute,omitempty"`
+	Match            string      `json:"match,omitempty"`
+	Value            interface{} `json:"value,omitempty"`
+	Level            string      `json:"level,omitempty"`
+	ComparisonType   string      `json:"comparison_type,omitempty"`
+	Time             string      `json:"time,omitempty"`
+	TargetType       string      `json:"targetType,omitempty"`
+	TargetIdentifier string      `json:"targetIdentifier,omitempty"`
+	Key              string      `json:"key,omitempty"`
 }
 
 type AlertRule struct {
@@ -41,9 +66,12 @@ type AlertRule struct {
 	DateCreated *time.Time           `json:"dateCreated,omitempty"`
 	Environment *string              `json:"environment,omitempty"`
 	ActionMatch alertRuleMatchPolicy `json:"actionMatch,omitempty"`
+	FilterMatch alertRuleMatchPolicy `json:"filterMatch,omitempty"`
 	Frequency   uint                 `json:"frequency,omitempty"` // run actions at most once every Frequency minutes
 	Conditions  []AlertRuleCondition `json:"conditions,omitempty"`
+	Filters     []AlertFilters       `json:"filters,omitempty"`
 	Actions     []AlertRuleAction    `json:"actions,omitempty"`
+	Owner       string               `json:"owner,omitempty"`
 }
 
 func (c *Client) GetAlertRules(o Organization, p Project) ([]AlertRule, *Link, error) {
